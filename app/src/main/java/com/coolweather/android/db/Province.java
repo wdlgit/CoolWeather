f735ebc0cba23e55bdce19a07e5d26489db47a7e
wdlgit <wdlgit@github.com>
2020-04-10 10:45:18 +0000

    add fragment

--- app/src/main/java/com/coolweather/android/db/Province.java
+++ app/src/main/java/com/coolweather/android/db/Province.java
@@ -10,18 +10,23 @@ public class Province extends DataSupport {
     public int getId(){
         return id;
     }
+
     public void setId(int id){
         this.id=id;
     }
+
     public int getProvinceCode(){
         return provinceCode;
     }
+
     public void setProvinceCode(int provinceCode){
         this.provinceCode=provinceCode;
     }
+
     public String getProvinceName(){
         return provinceName;
     }
+
     public void setProvinceName(String provinceName){
         this.provinceName=provinceName;
     }
